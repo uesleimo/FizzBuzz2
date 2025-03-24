ee1b63c79d4785ab37b45311f8bf23840b45421a
uesleimo <2023182@student.cct.ie>
2025-03-24 13:27:43 +0000

    Changes

--- FizzBuzz2/src/fizzbuzz2/FizzBuzz2.java
+++ FizzBuzz2/src/fizzbuzz2/FizzBuzz2.java
@@ -22,6 +22,20 @@ public class FizzBuzz2 {
         If a number is divisible by both, print FizzBuzz
         
         */
+ 
+        for (int i = 1; i <= 150; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
-    
 }
+
+    
+
